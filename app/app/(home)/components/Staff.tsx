fc4d9cdece732cace5e0e789fb8e367a0d5f857d
david zambrano <david._101@hotmail.com>
2024-06-13 16:50:59 -0500

    add

--- app/app/(home)/components/Staff.tsx
+++ app/app/(home)/components/Staff.tsx
@@ -2,51 +2,47 @@
 import React from "react";
 import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 import Title from "./Title";
+
 const people = [
   {
     id: 1,
     name: "David Zambrano",
     designation: "Co-founder/ Technical Lead",
-    image:
-      "/yo.jpg",
+    image: "/yo.jpg",
   },
   {
     id: 2,
     name: "Luis Zambrano",
     designation: "Co-founder/Project Manager",
-    image:
-      "/luis.jpg",
+    image: "/luis.jpg",
   },
   {
     id: 3,
     name: "Eva Palacios",
-    designation: "Produc Owner/Scrum Master",
-    image:
-      "/eva.jpg",
+    designation: "Product Owner/Scrum Master",
+    image: "/eva.jpg",
   },
   {
     id: 4,
     name: "Floris Del Salto",
     designation: "UX Designer",
-    image:
-      "/floria.jpg",
+    image: "/floria.jpg",
   },
   {
     id: 5,
     name: "Javier Roca",
     designation: "Senior Developer",
-    image:
-      "/roca.jpg",
+    image: "/roca.jpg",
   },
 ];
 
 export function AnimatedTooltipPreview() {
   return (
-    <div className="py-10 p-5 sm:p-0 m-40">  
-    <Title text="Meet the team"></Title>
-    <div className="flex flex-row items-center justify-center mb-10 w-full">
-      <AnimatedTooltip items={people} />
-    </div>
+    <div className="py-10 px-5 sm:px-10 lg:px-20 xl:px-40">
+      <Title text="Meet the team" />
+      <div className="flex flex-wrap items-center justify-center mb-10 w-full">
+        <AnimatedTooltip items={people} />
+      </div>
     </div>
   );
 }
